302066215c84fe5cb5e806056fcc59fd18f9cca7
RAGNAROKGoW <mypersonal19032005@gmail.com>
2024-12-20 20:00:51 +0530

    Reverse the pattern made above.

--- Test_3.java
+++ Test_3.java
@@ -9,5 +9,27 @@ class Test_3{
             }
             System.out.println();
         }
+        // *
+        // * *
+        // * * *
+        // * * * *
+
+        System.out.println();
+
+        for(row=0; row<=3; row++){
+            for(col=0; col<3-row; col++){
+                System.out.print("  ");
+            }
+            for(col=0; col<=row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        //       *
+        //     * *
+        //   * * *
+        // * * * *
+            
     }
 }
