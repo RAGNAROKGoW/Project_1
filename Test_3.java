4a5d14829c50bfbaa867f4b86156f3d209cdd8d4
RAGNAROKGoW <mypersonal19032005@gmail.com>
2024-12-20 21:37:07 +0530

    Make a pattern of square made up of stars tilted at an angle of 45 deg.

--- Test_3.java
+++ Test_3.java
@@ -45,6 +45,47 @@ class Test_3{
         //  * *
         // * * *
         //* * * *
+
+        System.out.println();
+
+        for(row=0; row<3; row++){
+            for(col=0; col<3-(row+1); col++){
+                System.out.print("  ");
+            }
+            for(col=0; col<1; col++){
+                System.out.print("* ");
+            }
+            if(row==0){
+                System.out.println();
+                continue;
+            }
+            for(col=0; col<2*(row-1)+1; col++){
+                System.out.print("  ");
+            }
+            for(col=0; col<1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(row=1; row>=0; row--){
+            for(col=0; col<3-(row+1); col++){
+                System.out.print("  ");
+            }
+            for(col=0; col<1; col++){
+                System.out.print("* ");
+            }
+            if(row==0){
+                System.out.println();
+                continue;
+            }
+            for(col=0; col<2*(row-1)+1; col++){
+                System.out.print("  ");
+            }
+            for(col=0; col<1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
             
     }
 }
